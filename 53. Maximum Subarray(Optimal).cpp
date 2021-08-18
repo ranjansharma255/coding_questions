3b27880b2667eaa275797f769b7fb97564693be5
Ranjan Sharma <ranjansharma255@gmail.com>
2021-08-18 13:02:17 +0530

    Added Optimal

---
+++ 53. Maximum Subarray(Optimal).cpp
@@ -0,0 +1,23 @@
+// Kadane's Algorithm
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+// here at the maxSum = 0 statement this would not work if all the elements in the array are negative.
+// so to make it work we can initialize the maxSum = INT_MIN or -infinity
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = INT_MIN;
+        int curSum = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            curSum = curSum + nums[i];
+            if(curSum > maxSum) {
+                maxSum = curSum;
+            }
+            if(curSum < 0) {
+                curSum = 0;
+            }
+        }
+        return maxSum;
+    }
+};
