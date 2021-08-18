90a6a9582d33e663b8a620adc8397c8c928b0268
Ranjan Sharma <ranjansharma255@gmail.com>
2021-08-18 13:16:02 +0530

    Added Comments

--- 53. Maximum Subarray(Optimal).cpp
+++ 53. Maximum Subarray(Optimal).cpp
@@ -1,5 +1,5 @@
 // Kadane's Algorithm
-// Time Complexity: O(N)
+// Time Complexity: O(N) Linear Iteration
 // Space Complexity: O(1)
 // here at the maxSum = 0 statement this would not work if all the elements in the array are negative.
 // so to make it work we can initialize the maxSum = INT_MIN or -infinity
@@ -21,3 +21,5 @@ public:
         return maxSum;
     }
 };
+// Related Resources
+// https://afteracademy.com/blog/maximum-subarray-sum
