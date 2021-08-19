c160da2182fd805c8a407330f3e5241a96c193df
Ranjan Sharma <ranjansharma255@gmail.com>
2021-08-19 13:23:50 +0530

    added optimal

---
+++ 152. Maximum Product Subarray(Optimal).cpp
@@ -0,0 +1,20 @@
+//Time Complexity: O(N)
+//Space Complexity: O(1)
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int minvalue = nums[0];
+        int maxvalue = nums[0];
+        int product = nums[0];
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[i] < 0)
+                swap(minvalue, maxvalue); //for array swap function has N complexity
+
+            maxvalue = max(nums[i], maxvalue * nums[i]);
+            minvalue = min(nums[i], minvalue * nums[i]);
+            if(maxvalue > product)
+                product = maxvalue;
+        }
+        return product;
+    }
+};
