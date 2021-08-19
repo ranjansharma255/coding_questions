9cbe32901c639e2640263b78b590ec8b539df93a
Ranjan Sharma <ranjansharma255@gmail.com>
2021-08-19 20:32:01 +0530

    added new approach

--- cpp/1295. Find Numbers with Even Number of Digits.cpp
+++ cpp/1295. Find Numbers with Even Number of Digits.cpp
@@ -61,3 +61,16 @@ public:
         return cnt;
     }
 };
+
+//another Approach
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int count = 0;
+        for(int a : nums){
+            string temp = to_string(a);
+            count += (temp.size() % 2 == 0);
+        }
+        return count;
+    }
+};
