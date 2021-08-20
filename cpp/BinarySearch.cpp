aa6765baa3bc51581f8d5e856d18b61295276a3a
Ranjan Sharma <ranjansharma255@gmail.com>
2021-08-20 12:47:33 +0530

    Added java Implementation

--- cpp/BinarySearch.cpp
+++ cpp/BinarySearch.cpp
@@ -27,3 +27,36 @@ int main() {
 // if we donot pass the size of the array in the binarySearch function
 // and if we calculate the size of the array using sizeof() in the binarySearch function then it will give warning
 // why because when we pass and array to a function then it loses its sense of how big it is.
+
+// to solve this easiest way is to have a parameter size in the function call
+
+// java implementation
+package com.company;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 5, 15, 16, 20, 60, 100};
+        int target = 10;
+        int ans = binarySearch(arr, target);
+        System.out.println(ans);
+    }
+
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end) {
+            //find the middle element
+            int mid = (start + end) / 2;
+
+            if(target < arr[mid]) {
+                end = mid - 1;
+            } else if(target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // ans found
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
