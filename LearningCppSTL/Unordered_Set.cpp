bb186901d41afe853aee75d766cfbac05eb79c2e
Ranjan Sharma <ranjansharma255@gmail.com>
2021-10-03 19:41:23 +0530

    added comments

---
+++ LearningCppSTL/Unordered_Set.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unordered_set>
+#define nline '\n'
+
+using namespace std;
+
+int main()
+{
+	unordered_set<int> s;
+	s.insert(10);
+	s.insert(12);
+	for (auto it : s) {
+		cout << it << nline;
+	}
+	
+	if(s.find(10) == s.end()) {
+		cout << "Not found" << nline;
+	} else {
+		cout << "Found" << nline;
+	}
+	
+	cout << "size of the set is " << s.size();
+	
+	s.clear();
+	
+	return 0;
+}
