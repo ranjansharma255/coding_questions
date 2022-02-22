ed9cd8e9b5e9f113f615baf895eb7daf38a4a483
Ranjan Sharma <ranjansharma255@gmail.com>
2022-02-22 20:01:21 +0530

    Update 744. Find Smallest Letter Greater Than Target(Optimal).cpp

--- cpp/744. Find Smallest Letter Greater Than Target(Optimal).cpp
+++ cpp/744. Find Smallest Letter Greater Than Target(Optimal).cpp
@@ -12,7 +12,7 @@ class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
         int start = 0;
-        int end = letters.size();
+        int end = letters.size() - 1;
         while(start <= end) {
             int mid = start + (end - start) / 2;
 
